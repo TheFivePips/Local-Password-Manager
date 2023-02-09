07760bd858aa655a9ca99ada95e1f00c71e93f27
Sam Gardner <samgardner20@gmail.com>
2023-02-09 10:40:14 -0500

    added in a find_password function to search the local data file

--- main.py
+++ main.py
@@ -66,6 +66,25 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- Find Password ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Not Found", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="NO Details", message=f"No details for {website} exists")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -99,7 +118,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=42, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-search_button = Button(text="Search", width=15)
+search_button = Button(text="Search", width=15, command=find_password)
 search_button.grid(row=1, column=2)
 
 window.mainloop()
