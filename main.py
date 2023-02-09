b3dc9cedf3371ceba84d6f9447f3ff63868bfd61
Sam Gardner <samgardner20@gmail.com>
2023-02-09 10:22:04 -0500

    changed local data type and added some error handling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
+import random
 from tkinter import *
 from tkinter import messagebox
-import random
+
 import pyperclip
 
 
@@ -34,23 +36,34 @@ def generate_password():
     pyperclip.copy(password)
 
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
+    new_data = {website: {
+        "email": email,
+        "password": password
+    }}
+
     if len(website) == 0 or len(password) == 0:
-        is_present = messagebox.showinfo(title="Oops", message="Please make sure you haven't left any field empty")
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any field empty")
     else:
-        is_okay = messagebox.askokcancel(title=website,
-                                         message=f"These are the details entered: \nEmail: {email} \nPassword: {password}\nIs it okay to save?")
-        if is_okay:
-            with open("data.text", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -72,8 +85,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3)
 
 # entries
-website_entry = Entry(width=45)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=25)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=45)
 email_entry.insert(0, "samgardner20@gmail.com")
@@ -82,9 +95,11 @@ password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
 # buttons
-generate_password_button = Button(text="Generate Password",command=generate_password)
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=42, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=15)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
